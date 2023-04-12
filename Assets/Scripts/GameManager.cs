698b7b3c80aacea71e12482de873dda9766e2a21
devonnc <dechavez@s.sfusd.edu>
2023-04-12 11:50:00 -0700

    finished unit 5.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
    
 
@@ -12,6 +14,9 @@ public class GameManager : MonoBehaviour
     void Start() 
     {
         StartCoroutine(SpawnTarget()); 
+        score = 0;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -27,8 +32,16 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(spawnRate);
         int index = Random.Range(0, targets.Count);
         Instantiate(targets[index]);
+
         }
        
     }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    
 
 }
