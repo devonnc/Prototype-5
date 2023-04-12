2c6de3755f8ef65905cd4fbb7cceab247c8ce308
devonnc <dechavez@s.sfusd.edu>
2023-04-12 10:54:33 -0700

    finished 5.1

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,15 +11,16 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = 6;
+   
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         
-        targetRb.AddForce(Vector3.up * Random.Range(minSpped, maxSpeed), ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         
-        transform.position = new Vector3(Random.Range(-xRange, xRange, -ySpawnPos);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
@@ -28,15 +29,31 @@ public class Target : MonoBehaviour
         
     }
 
+     private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
-    flaot RandomTorque()
+    float RandomTorque()
     {
         return Random.Range(-maxTorque, maxTorque);
     }
 
+    Vector3 RandomSpawnPos()
+    {
+       return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+
+    
 }
 
